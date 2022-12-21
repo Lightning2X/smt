ed2aea4307e3ebabb2c7c88af49f45df8fd69a9c
Pascal Tollens <p.y.tollens@students.uu.nl>
2022-12-21 23:18:27 +0100

    --Hotfix-- Player could move in any direction  they look at. Fixed by setting the y value to 0. Since cam controls are gonna be similar to the original doom anyway, also removed looking up and down.

--- Assets/Scripts/Camera_Logic.cs
+++ Assets/Scripts/Camera_Logic.cs
@@ -10,19 +10,30 @@ public class Camera_Logic : NetworkBehaviour
     [SerializeField] private Transform localPlayerOrientation;
     private float sensitivtyX = 1000, sensitivityY = 1000;
     private float rotationX, rotationY;
+    bool cursorLocked = false;
     // Update is called once per frame
     void Update()
     {
         if (localPlayer == null)
             return;
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            if (cursorLocked)
+                Cursor.lockState = CursorLockMode.None;
+            else
+                Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = cursorLocked;
+            cursorLocked = !cursorLocked;
+            Debug.Log("up");
+        }
         if (!Cursor.visible)
         {
             rotationY += Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensitivityY; //rotation around Y axis
-            rotationX -= Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensitivtyX; //rotation around X axis (potentially remove this DOF, since we need to keep aiming simple
-            rotationX = Mathf.Clamp(rotationX, -90, 90);
+            //rotationX -= Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensitivtyX; //rotation around X axis (potentially remove this DOF, since we need to keep aiming simple
+            //rotationX = Mathf.Clamp(rotationX, -90, 90);
             //rotationX = m
             //transform.rotation = Quaternion.Euler(rotationX, rotationY, 0);
-            localPlayer.rotation = Quaternion.Euler(rotationX, rotationY, 0); //the entire model also looks up, just a placeholder
+            localPlayer.rotation = Quaternion.Euler(0, rotationY, 0); //the entire model also looks up, just a placeholder
         }
 
         transform.rotation = localPlayer.rotation;
