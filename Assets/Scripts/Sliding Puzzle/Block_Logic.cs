5997670fb18a19c7c050deb817b00c561c2ce077
Daannado <Daanvdv2000@outlook.com>
2023-01-14 17:13:20 +0100

    sliding puzzle prefab fixed

--- Assets/Scripts/Sliding Puzzle/Block_Logic.cs
+++ Assets/Scripts/Sliding Puzzle/Block_Logic.cs
@@ -1,22 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Block_Logic : MonoBehaviour
 {
+    public RectTransform rectTransform;
     public Vector3 targetPos;
     public Vector3 correctPos;
     // Start is called before the first frame update
     void Start()
     {
-        targetPos = transform.position;
-        correctPos = transform.position;
+        targetPos = rectTransform.localPosition;
+        correctPos = rectTransform.localPosition;
         gameObject.tag = "SlidingPuzzle_Block";
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, targetPos, 0.2f);
+        rectTransform.localPosition = Vector3.Lerp(rectTransform.localPosition, targetPos, 0.1f);
     }
 }
--- Assets/Scripts/Sliding Puzzle/SlidePuzzle_Logic.cs
+++ Assets/Scripts/Sliding Puzzle/SlidePuzzle_Logic.cs
@@ -1,57 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SlidePuzzle_Logic : MonoBehaviour
 {
-    [SerializeField] private Transform emptySpace = null;
+    [SerializeField] private RectTransform emptySpace = null;
     private Camera cam;
     private GameObject[] blocks;
     private float swapDistance = 0.5f;
+    private int inversions;
+    private int[] location;
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
         blocks = GameObject.FindGameObjectsWithTag("SlidingPuzzle_Block");
-        float height = blocks[1].GetComponent<SpriteRenderer>().bounds.size.x;
-        swapDistance = height;
-        Shuffle();
+        location = new int[blocks.Length - 1];
+        swapDistance = blocks[1].GetComponent<RectTransform>().rect.width;
+        for(int x = 0; x < location.Length; x++)
+            {location[x] = x;}
+        Invoke("Shuffle", 0.1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
-        {
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
-            if(hit)
-            {
-                double dis = Vector2.Distance(emptySpace.position, hit.transform.position);
-                if(dis <= swapDistance)
-                {
-                    Vector2 lastEmptySpacePos = emptySpace.position;
-                    Block_Logic thisBlock = hit.transform.GetComponent<Block_Logic>();
-                    emptySpace.position = thisBlock.targetPos;
-                    thisBlock.targetPos = lastEmptySpacePos;
-                }
-            }
-            if(Correct()) //should be extended with the wanted functionality for succeding the puzzle
-                Debug.Log("Woohoo");
-        }
     }
 
-    //shuufles all the blocks on the board except for the empty space
+    public void TileMove(GameObject block)
+    {
+        Block_Logic thisBlock = block.transform.GetComponent<Block_Logic>();
+        double dis = Vector2.Distance(emptySpace.localPosition, thisBlock.targetPos);
+
+        if(dis <= swapDistance)
+        {
+            Vector2 lastEmptySpacePos = emptySpace.localPosition;
+            emptySpace.localPosition = thisBlock.targetPos;
+            thisBlock.targetPos = lastEmptySpacePos;
+        }
+
+        if(Correct()) //should be extended with the wanted functionality for succeding the puzzle
+            Debug.Log("Woohoo");
+    }
+
+    //shuffles all the blocks on the board except for the empty space
     public void Shuffle()
     {
         for(int n = 1; n < blocks.Length; n++)
         {
             int rnd = Random.Range(1, blocks.Length);
-            Vector2 safePos = blocks[n].GetComponent<Block_Logic>().targetPos;
-            Vector2 testPos = blocks[rnd].GetComponent<Block_Logic>().targetPos;
+            Vector3 safePos = blocks[n].GetComponent<Block_Logic>().targetPos;
+            Vector3 testPos = blocks[rnd].GetComponent<Block_Logic>().targetPos;
             blocks[n].GetComponent<Block_Logic>().targetPos = testPos;
             blocks[rnd].GetComponent<Block_Logic>().targetPos = safePos;
+            int holder = location[n-1];
+            location[n-1] = location[rnd-1];
+            location[rnd-1] = holder;
         }
+
+        if(CheckInversions() % 2 != 0) Shuffle();
     }
 
     //Checks if the current board is correct by checking if every piece is in correct position, return true or false accordingly
@@ -64,5 +72,22 @@ public class SlidePuzzle_Logic : MonoBehaviour
             return false;
         }
         return true;
+    }
+    
+    //function to check the number of inversions
+    public int CheckInversions()
+    {
+        inversions = 0;
+        for (int i = 0; i < location.Length; i++)
+        {
+            for (int j = i + 1; j < location.Length; j++)
+            {
+                if (location[i] > location[j])
+                {
+                    inversions++;
+                }
+            }
+        }
+        return inversions;
     }
 }
