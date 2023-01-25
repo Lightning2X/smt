4d0d198d6a941f92f80f37687a0e10ad71e8cb6a
ptollens <pytollens@gmail.com>
2023-01-25 22:57:50 +0100

    /!\ Doesn't work yet, don't use this branch /!\

--- Assets/Scripts/UI_NetworkManager_Menu.cs
+++ Assets/Scripts/UI_NetworkManager_Menu.cs
@@ -1,22 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+using Unity.Netcode.Transports.UTP;
 
 public class UI_NetworkManager_Menu : MonoBehaviour
 {
     [SerializeField] private GameObject networkMenu;
     private bool menuActive = false;
-    [SerializeField] private Button serverButton;
+    //[SerializeField] private Button serverButton;
     [SerializeField] private Button hostButton;
     [SerializeField] private Button clientButton;
+    [SerializeField] private TextMeshProUGUI ipAddressText;
+    [SerializeField] private TextMeshProUGUI joinIPAddressText;
+    string ipAddress;
+
+    public string GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                ipAddressText.text = ip.ToString();
+                ipAddress = ip.ToString();
+                return ip.ToString();
+            }
+        }
+        throw new System.Exception("No network adapters with an IPv4 address in the system!");
+    }
 
     void Awake()
     {
-        serverButton.onClick.AddListener(() => { NetworkManager.Singleton.StartServer(); });
+        //serverButton.onClick.AddListener(() => { NetworkManager.Singleton.StartServer(); });
+        GetLocalIPAddress();
+        NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectionData.Address = ipAddress;
         hostButton.onClick.AddListener(() => { NetworkManager.Singleton.StartHost(); });
-        clientButton.onClick.AddListener(() => { NetworkManager.Singleton.StartClient(); });
+        //clientButton.onClick.AddListener(() => { NetworkManager.Singleton.StartClient(); });
+    }
+    public void joinHost()
+    {
+        if(joinIPAddressText.text != "")
+        ipAddress = joinIPAddressText.text;
+        Debug.Log(ipAddress);
+        NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectionData.Address = ipAddress;
+        NetworkManager.Singleton.StartClient();
     }
     public void ToggleMenu()
     {
